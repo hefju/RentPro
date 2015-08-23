ac3603c9829f4f9b604b2014f12b6a4713706954
hefju <hefju@live.cn>
2015-08-23 22:48:14 +0800

    js登录代码例子

--- src/route/login.go
+++ src/route/login.go
@@ -7,6 +7,16 @@ type Login struct {
     User     string `form:"user" json:"user" binding:"required"`
     Password string `form:"password" json:"password" binding:"required"`
 }
+
+/* js登录代码
+function getEncryption(password, uin, vcode, isMd5) {
+var str1 = hexchar2bin(isMd5 ? password : md5(password));
+var str2 = md5(str1 + uin);
+var str3 = md5(str2 + vcode.toUpperCase());
+return str3
+}
+*/
+
 func (login *Login)LoginJSON(c *gin.Context){
     var json Login
     if c.BindJSON(&json) == nil {
